ac5b260555377fc29a64cbefb05316f9d2572532
User <user@macbook-masha-a.local>
2018-06-04 20:44:05 +0300

    Initial Commit

---
+++ PdfReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PdfReader
+//
+//  Created by User on 04.06.2018.
+//  Copyright © 2018 Garon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
